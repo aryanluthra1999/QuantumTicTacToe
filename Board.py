0788a9849e1d5d0d24db2c0fe9c1f4bf6b2e42f8
Aryan Luthra <aryanluthra@berkeley.edu>
2020-04-12 19:50:57 +0100

    added no cachefiles to gitignore

--- Board.py
+++ Board.py
@@ -38,12 +38,17 @@ class QBoard:
 
     def make_move(self, type, *args):
         assert type == "collapse" or type == "place"
+        if type == "place":
+            self.place_move(args)
+        else:
+            self.collapse(args)
 
     def place_move(self, player, loc1, loc2):
         assert not self.is_win(), "Game already won"
         assert player == 'x' or player == 'o', "not valid symbol for player"
         assert (loc1 not in self.measured) and 1 <= loc1 <= 9, "location to play not on the board"
         assert (loc2 not in self.measured) and 1 <= loc2 <= 9, "location to play not on the board"
+        assert not self.cycle
         self.moves += 1
         self.cells[loc1 - 1].add(player + str(self.moves))
         self.cells[loc2 - 1].add(player + str(self.moves))
@@ -52,12 +57,13 @@ class QBoard:
     def collapse(self, player, loc, target_player, move_num):
         assert target_player + str(move_num) in self.cells[loc - 1]
         assert loc not in self.measured
+        assert self.cycle
         # TODO: implement collapse dynamics
         self.measured[loc] = target_player
 
     def detect_cycle(self):
         try:
-            return nx.find_cycle(self.graph)
+            self.cycle = nx.find_cycle(self.graph)
         except nx.NetworkXNoCycle:
             return None
 
