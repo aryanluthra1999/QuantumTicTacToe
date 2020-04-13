3651b25108114fea2b6ab995e4224fcd5a18e946
Aryan Luthra <aryanluthra@berkeley.edu>
2020-04-13 02:21:58 +0100

    Update GameState.py

--- GameState.py
+++ GameState.py
@@ -1,3 +1,17 @@
+from Board import QBoard
+from Player import Player
+
 class GameState:
 
     def __init__(self):
+        self.board = QBoard()
+        self.playerx = Player('x')
+        self.playero = Player('o')
+        self.curr_turn = 'x'
+        
+
+    @staticmethod
+    def assign_players():
+        #TODO: make a random assigner of players
+        return p1, p2
+
