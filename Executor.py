f95393f3e8adff55dab5d66bf4701ea61d4f1cbc
Aryan Luthra <aryanluthra@berkeley.edu>
2020-04-15 05:29:40 +0100

    Update Executor.py

--- Executor.py
+++ Executor.py
@@ -10,15 +10,15 @@ def changeGameState(pressedButtonNumber):
     change_button(pressedButtonNumber, "kavi")
     pass
 
+if __name__ == '__main__':
+    display = tk.Tk()
+    display.title("Quantum Tic Tac Toe")
+    display.grid()
+    tiles = []
+    for tile_number in range(9):
+        curr_btn = tk.Button(display, text="", width="20",height="2", border="2",
+        command=lambda tile_number = tile_number: changeGameState(tile_number))
+        curr_btn.grid(row=tile_number%3, column=tile_number//3)
+        tiles.append(curr_btn)
 
-display = tk.Tk()
-display.title("Quantum Tic Tac Toe")
-display.grid()
-tiles = []
-for tile_number in range(9):
-    curr_btn = tk.Button(display, text="", width="20",height="2", border="2",
-    command=lambda tile_number = tile_number: changeGameState(tile_number))
-    curr_btn.grid(row=tile_number%3, column=tile_number//3)
-    tiles.append(curr_btn)
-
-display.mainloop()
+    display.mainloop()
