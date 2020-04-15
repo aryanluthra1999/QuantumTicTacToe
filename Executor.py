8eb8fdd6706fb66298af3465f744f168bb42a0a8
kavibvaidya <kavid.vaidya@berkeley.edu>
2020-04-15 10:35:48 -0700

    backend bugs halt progress

--- Executor.py
+++ Executor.py
@@ -1,14 +1,32 @@
 from GameState import GameState
 import tkinter as tk
 
-def change_button(number, new_value):
-    tiles[number].configure(text=new_value)
+def change_button(number):
+    print("changing state with ", number)
+    tiles[number].configure(text=", ".join(state.board.cells[number]))
 
 def changeGameState(pressedButtonNumber):
-    # change the GameState
-    # Get the last change
-    change_button(pressedButtonNumber, "kavi")
-    pass
+        global last_move
+        print(f"last move {last_move} and curr {pressedButtonNumber}")
+        cycles = state.board.detect_cycle()
+        won = False #state.board.is_win()
+        if cycles and not won:
+            # Collapse
+            pass
+        elif not won:
+            # Add state
+            print("no cycles")
+            if last_move and last_move != pressedButtonNumber: #otherwise we need to wait for another turn.
+                try:
+                    state.board.make_move("place", last_move, pressedButtonNumber)
+                    change_button(last_move)
+                    change_button(pressedButtonNumber)
+                    last_move = None
+                except AssertionError:
+                    print("an error was raised", e)
+                return
+            last_move = pressedButtonNumber
+
 
 if __name__ == '__main__':
     display = tk.Tk()
@@ -18,7 +36,8 @@ if __name__ == '__main__':
     for tile_number in range(9):
         curr_btn = tk.Button(display, text="", width="20",height="2", border="2",
         command=lambda tile_number = tile_number: changeGameState(tile_number))
-        curr_btn.grid(row=tile_number%3, column=tile_number//3)
+        curr_btn.grid(row=tile_number//3, column=tile_number%3)
         tiles.append(curr_btn)
-
+    state = GameState()
+    last_move = None
     display.mainloop()
