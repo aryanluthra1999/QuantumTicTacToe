4e18ec7b64becbeccdb29300364db026ba8820af
Aryan Luthra <aryanluthra@berkeley.edu>
2020-04-17 00:43:27 +0100

    Update Board.py

--- Board.py
+++ Board.py
@@ -12,6 +12,7 @@ class QBoard:
         self.graph.add_nodes_from(range(1, 10))
         self.cycle = None
         self.curr_turn = 'x'
+        self.move_locs = dict()
 
     def is_win(self):
         wins = [[1, 2, 3],
@@ -65,23 +66,28 @@ class QBoard:
         assert (loc1 not in self.measured) and 1 <= loc1 <= 9, "location to play not on the board"
         assert (loc2 not in self.measured) and 1 <= loc2 <= 9, "location to play not on the board"
         assert not self.cycle
+        assert loc1 not in self.cycle.nodes and loc2 not in self.cycle.nodes
+
         self.moves += 1
-        self.cells[loc1 - 1].add(str(self.curr_turn) + str(self.moves))
-        self.cells[loc2 - 1].add(str(self.curr_turn) + str(self.moves))
+        move_str = str(self.curr_turn) + str(self.moves)
+        self.cells[loc1 - 1].add(move_str)
+        self.cells[loc2 - 1].add(move_str)
         self.graph.add_edge(loc1, loc2)
+        self.move_locs[move_str] = (loc1, loc2)
 
     def collapse(self, loc, move_str):
         assert move_str in self.cells[loc - 1]
         assert loc not in self.measured
+        assert loc in self.cycle.nodes
+        assert self.move_locs[move_str][0] in self.cycle.nodes and self.move_locs[move_str][1] in self.cycle.nodes
         assert self.cycle
-        self.measured[loc] = move_str[0]
 
-        # TODO: implement collapse dynamics
+        self.collapse_helper(loc, move_str)
 
         for loc in self.measured:
             if loc in self.graph.nodes:
                 self.graph.remove_node(loc)
-                self.cells[loc-1] = set()
+                self.cells[loc - 1] = set()
 
         for cell in self.cells:
             if move_str in cell:
@@ -89,12 +95,25 @@ class QBoard:
 
         self.detect_cycle()
 
+    def collapse_helper(self, loc, move_str):
+
+        self.measured[loc] = move_str[0].upper()
+        # TODO: implement collapse dynamics
+        self.cells[loc].remove(move_str)
+
+        for other_move in self.cells[loc]:
+            if other_move == move_str or other_move in self.measured.values():
+                continue
+            for other_loc in self.move_locs[other_move]:
+                if other_loc not in self.measured:
+                    self.collapse_helper(other_loc, other_move)
+
     def detect_cycle(self, label=None):
         try:
             self.cycle = nx.find_cycle(self.graph)
             if label:
                 label.configure(text="You will now have to collapse the system"
-                + " enter the item u want to measure and click the block")
+                                     + " enter the item u want to measure and click the block")
         except nx.NetworkXNoCycle:
             self.cycle = None
 
