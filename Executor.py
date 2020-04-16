0980f233d7ffca6a5660f9a7e081e2245620db05
kavibvaidya <kavid.vaidya@berkeley.edu>
2020-04-15 17:41:53 -0700

    made the non-cycle frontend

--- Executor.py
+++ Executor.py
@@ -1,5 +1,8 @@
 from GameState import GameState
 import tkinter as tk
+import matplotlib.pyplot as plt
+import matplotlib
+import networkx as nx
 
 def change_button(number):
     print("changing state with ", number)
@@ -18,26 +21,28 @@ def changeGameState(pressedButtonNumber):
             print("no cycles")
             if last_move and last_move != pressedButtonNumber: #otherwise we need to wait for another turn.
                 try:
-                    state.board.make_move("place", last_move, pressedButtonNumber)
+                    state.board.make_move("place", last_move + 1, pressedButtonNumber + 1)
                     change_button(last_move)
                     change_button(pressedButtonNumber)
                     last_move = None
-                except AssertionError:
+                except AssertionError as e:
                     print("an error was raised", e)
                 return
             last_move = pressedButtonNumber
 
 
 if __name__ == '__main__':
+    matplotlib.use("TkAgg")
     display = tk.Tk()
     display.title("Quantum Tic Tac Toe")
     display.grid()
     tiles = []
     for tile_number in range(9):
-        curr_btn = tk.Button(display, text="", width="20",height="2", border="2",
+        curr_btn = tk.Button(display, text="", width="20",height="5", border="2",
         command=lambda tile_number = tile_number: changeGameState(tile_number))
         curr_btn.grid(row=tile_number//3, column=tile_number%3)
         tiles.append(curr_btn)
     state = GameState()
+    nx.draw(state.board.graph)
     last_move = None
     display.mainloop()
