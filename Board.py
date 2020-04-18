8a7d1255b8973697b9e1621039cdff0a841c53e3
Aryan Luthra <aryanluthra@berkeley.edu>
2020-04-18 09:52:48 +0100

    Update Board.py

--- Board.py
+++ Board.py
@@ -50,6 +50,8 @@ class QBoard:
         self.detect_cycle()
         # print(self)
 
+        
+        
         assert type == "collapse" or type == "place"
         if type == "place":
             self.place_move(*args)
@@ -60,14 +62,12 @@ class QBoard:
         if not self.cycle:
             if self.curr_turn == 'x':
                 self.curr_turn = 'o'
-                if self.alg:
-                    print(self.minimax())
-                print("Switching Turns")
+                # print("Switching Turns")
             elif self.curr_turn == 'o':
                 self.curr_turn = 'x'
+                # print("Switching Turns")
                 if self.alg:
                     print(self.minimax())
-                print("Switching Turns")
             else:
                 assert self.curr_turn == 'x' or self.curr_turn == 'o', "not valid symbol for player"
 
@@ -77,7 +77,7 @@ class QBoard:
         assert (loc1 not in self.measured) and 1 <= loc1 <= 9, "location to play not on the board"
         assert (loc2 not in self.measured) and 1 <= loc2 <= 9, "location to play not on the board"
         assert self.cycle is None
-
+        
         self.moves += 1
         move_str = str(self.curr_turn) + str(self.moves)
         self.cells[loc1 - 1].add(move_str)
@@ -171,7 +171,7 @@ class QBoard:
         possible_moves = dict()
         for loc in possible_locs:
             curr_loc_collapse_moves = []
-            for move_str in self.cells[loc-1]:
+            for move_str in self.cells[loc - 1]:
                 move_str_locs = self.move_locs[move_str]
                 if move_str_locs[0] in possible_locs and move_str_locs[1] in possible_locs:
                     curr_loc_collapse_moves.append(move_str)
@@ -188,7 +188,6 @@ class QBoard:
         return successors
 
     @staticmethod
-
     def utility(board, depth):
         winner = board.is_win()
         if winner == 'X':
@@ -205,8 +204,8 @@ class QBoard:
             ret_util_successor_move = None
             print(depth)
             for successor_board, successor_move in board.get_succesors():
-                successor_util = 0.75*(QBoard.utility(successor_board, depth + 1)[0])
-                if depth ==1:
+                successor_util = 0.75 * (QBoard.utility(successor_board, depth + 1)[0])
+                if depth == 1:
                     if successor_util > ret_util:
                         ret_util = successor_util
                         ret_util_successor_move = successor_move
@@ -220,4 +219,61 @@ class QBoard:
 
     def minimax(self):
         # get successor with the max util
-        return QBoard.utility(self, 0)
+        return QBoard.minimax_helper(self, 4, True, float('-inf'), float('inf'))
+
+    def utility_maxer(board, depth):
+        winner = board.is_win()
+        if winner == 'X':
+            return (100, "win")
+        elif winner == 'O':
+            return (-100, "loss")
+        elif winner == "nobody":
+            return (0, "tie")
+        else:
+            # TODO: return utility over succesors here
+            if depth > 4:
+                return (0, "unknown")
+
+    @staticmethod
+    def minimax_helper(board, depth, isMaximizingPlayer, alpha, beta):
+
+        print(depth)
+
+        winner = board.is_win()
+        if winner == 'X':
+            return 100, "win"
+        elif winner == 'O':
+            return -100, "loss"
+        elif winner == "nobody":
+            return 0, "tie"
+        elif depth <= 0:
+            measured_vals = board.measured.values()
+            num_x = sum([1 for val in measured_vals if val == 'X'])
+            num_o = len(measured_vals) - num_x
+            return num_x - num_o, "unknown"
+
+        if isMaximizingPlayer:
+            bestVal = float('-inf')
+            bestMove = None
+            for successor_board, successor_move in board.get_succesors():
+                value = QBoard.minimax_helper(successor_board, depth - 1, False, alpha, beta)[0]
+                if value > bestVal:
+                    bestVal = value
+                    bestMove = successor_move
+                alpha = max(alpha, bestVal)
+                if beta <= alpha:
+                    break
+            return bestVal, bestMove
+
+        else:
+            bestVal = float('inf')
+            bestMove = None
+            for successor_board, successor_move in board.get_succesors():
+                value = QBoard.minimax_helper(successor_board, depth - 1, True, alpha, beta)[0]
+                if value < bestVal:
+                    bestVal = value
+                    bestMove = successor_move
+                beta = min(beta, bestVal)
+                if beta <= alpha:
+                    break
+            return bestVal, bestMove
